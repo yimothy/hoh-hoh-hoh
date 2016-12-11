fb45bf71adbc7171d5c9af5cab35aeb814460fde
Svetlin Mladenov <svem@gmx.com>
2016-12-11 17:15:50 -0500

    modified descriptionController

--- client/app/wishlist_test/accordion.js
+++ client/app/wishlist_test/accordion.js
@@ -1,1 +1,21 @@
-angular module('description', []);
+angular.module('description', ['ui.bootstrap'])
+
+.controller('descriptionController', function($scope, $http, $log) {
+  
+  $scope.groups = [
+    {
+      title: 'Dynamic Group Header - 1',
+      content: 'Dynamic Group Body - 1',
+      open: false
+    }
+  ];
+
+  $scope.addNew = function() {
+    $scope.groups.push({
+      title: 'ipod',
+      content: 'Dynamically added new one',
+      open: false
+    });
+  };
+});
+
