b2cbae00ed594b7fbbede3b8da08ca31074bea84
Yimothy <tim@Yimothys-MacBook-Pro.local>
2016-12-12 21:49:20 -0500

    Completed grabbing receiver name full stack

--- client/app/santa/room.js
+++ client/app/santa/room.js
@@ -3,28 +3,19 @@ angular.module('hoh.room', [])
 .controller('RoomController', function($scope, $routeParams, RoomFactory, Auth) {
   $scope.data = {};
   $scope.data.roomData = {};
-  $scope.data.receiver = [];
 
   Auth.getSessionData();
   let userID = Auth.user.id;
   let id = $routeParams.id;
 
-  // $scope.createSantas = function(userIdArray) {
-  //   let copy = Array.prototype.slice.call(userIdArray);
-  //
-  //   // Setting the receiver to the last index item to resolve an undesired permutation
-  //   for ( var i = 0, receiver = copy[copy.length-1]; i < userIdArray.length; i++ ) {
-  //       while ( receiver === userIdArray[i] || !receiver )
-  //           // Grab a random member from the userIdArray
-  //           receiver = copy[ Math.floor((Math.random() * copy.length)) ];
-  //       copy.splice( copy.indexOf(receiver), 1 );
-  //       $scope.data.santas.push(
-  //         {'santa_id': userIdArray[i].user_id,
-  //         'receiver_id': receiver.user_id,
-  //         'receiverName': receiver.username});
-  //       receiver = null;
-  //   }
-  // }
+  $scope.getReceiver = function(array) {
+    for(let i = 0; i < array.length; i++) {
+      console.log('CHECK THIS ONE: ', array[i]);
+      if(array[i].user_id === userID) {
+        $scope.data.receiver = array[i].receiver_id;
+      }
+    }
+  }
 
   $scope.getUsersInRoom = function(roomID) {
     Auth.getSessionData();
@@ -32,15 +23,17 @@ angular.module('hoh.room', [])
     let id = $routeParams.id;
     RoomFactory.getUsersInRoom(userID, id)
     .then(function(users) {
-      console.log('THESE ARE USERS IN THE FRONT END: ', users.data);
       $scope.data.roomData.users = users.data;
+      console.log('THESE ARE USERS IN THE FRONT END: ', $scope.data.roomData.users);
+      $scope.getReceiver(users.data);
     })
+    // .then(function() {
+    // })
     // .then(function() {
     //   $scope.createSantas($scope.data.roomData.users);
     //   console.log('THIS IS SANTAS DATA IN FRONT: ', $scope.data.santas);
     //   RoomFactory.saveSantas(id, $scope.data.santas);
     // })
-
 
   }
 
