024c0bc6fa70d2b1746cd1c9c1261fcc179d5639
Yimothy <tim@Yimothys-MacBook-Pro.local>
2016-12-13 14:53:48 -0500

    Final push before presentation

--- client/app/santa/room.js
+++ client/app/santa/room.js
@@ -1,6 +1,6 @@
 angular.module('hoh.room', [])
 
-.controller('RoomController', function($scope, $routeParams, RoomFactory, Auth) {
+.controller('RoomController', function($scope, $routeParams, RoomFactory, Auth, Wishlist,Item) {
   $scope.data = {};
   $scope.data.roomData = {};
 
@@ -10,44 +10,52 @@ angular.module('hoh.room', [])
 
   $scope.getReceiver = function(array) {
     for(let i = 0; i < array.length; i++) {
-      console.log('CHECK THIS ONE: ', array[i]);
+      // console.log('CHECK THIS ONE: ', array[i]);
       if(array[i].user_id === userID) {
         return array[i];
       }
     }
   }
 
-  $scope.getUsersInRoom = function(roomID) {
+  $scope.getUsersInRoom = function() {
     Auth.getSessionData();
     let userID = Auth.user.id;
     let id = $routeParams.id;
     RoomFactory.getUsersInRoom(userID, id)
     .then(function(users) {
       $scope.data.roomData.users = users.data;
-      console.log('THESE ARE USERS IN THE FRONT END: ', $scope.data.roomData.users);
+      // console.log('THESE ARE USERS IN THE FRONT END: ', $scope.data.roomData.users);
       return $scope.getReceiver(users.data);
     })
     .then(function(receiverData) {
-      console.log('THIS IS THE RECEIVERDATA: ', receiverData);
+      // console.log('THIS IS THE RECEIVERDATA: ', receiverData);
       $scope.data.receiverID = receiverData.receiver_id;
       $scope.data.receiverName = receiverData.receiverName;
-
+      return $scope.data.receiverID;
     })
-
-    // .then(function() {
-    // })
-    // .then(function() {
-    //   $scope.createSantas($scope.data.roomData.users);
-    //   console.log('THIS IS SANTAS DATA IN FRONT: ', $scope.data.santas);
-    //   RoomFactory.saveSantas(id, $scope.data.santas);
-    // })
-
+    .then(function(receiverID) {
+      console.log('this is thE RECEIVER ID: ', receiverID);
+      return Wishlist.getListById(receiverID)
+    })
+    .then(function(wishlist) {
+      $scope.data.wishlist = wishlist[0];
+            console.log('this is thE WISHLIST: ', wishlist[0]);
+      return Item.getAllItems(wishlist[0].id);
+    })
+    .then(function(items) {
+      console.log('THESE ARE THE ITEMS: ', items);
+      $scope.data.wishlist.items = items;
+    })
   }
 
-  $scope.getUsersInRoom(userID, id);
+  $scope.getUsersInRoom();
 
 })
 .factory('RoomFactory', function($http) {
+  var getWishList = function(userID) {
+    return $http.get('/api/wishlist/' + userID);
+  }
+
   var getUsersInRoom = function(userID, roomID) {
     return $http.get('/api/santa/' + userID + '/' + roomID);
   }
@@ -58,6 +66,7 @@ angular.module('hoh.room', [])
 
   return {
     getUsersInRoom: getUsersInRoom,
-    saveSantas: saveSantas
+    saveSantas: saveSantas,
+    getWishList: getWishList
   }
 })
